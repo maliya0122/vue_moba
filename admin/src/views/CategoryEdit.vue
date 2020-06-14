49d5d3caa8dcfb97c0a98f38b2a2e0e5761dfd66
maliya <798039572@qq.com>
2020-06-14 22:06:07 +0800

    init

---
+++ admin/src/views/CategoryEdit.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="about">
+    <h1>新建分类</h1>
+    <el-form ref="form" :model="form" label-width="80px">
+      <el-form-item label="活动名称">
+        <el-input v-model="form.name"></el-input>
+      </el-form-item>
+
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">保存</el-button>
+        <el-button>取消</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        name: "",
+      },
+    };
+  },
+  methods: {
+    onSubmit(){
+
+    }
+  },
+};
+</script>
---
+++ admin/src/views/Main.vue
@@ -0,0 +1,58 @@
+<template>
+  <el-container style="height: 100vh">
+    <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+      <el-menu :default-openeds="['1', '3']" router>
+        <el-submenu index="1">
+          <template slot="title"
+            ><i class="el-icon-message"></i>分类管理</template
+          >
+          <el-menu-item-group>
+            <el-menu-item index="/category/create">新建分类</el-menu-item>
+            <el-menu-item index="/category/list">分类列表</el-menu-item>
+          </el-menu-item-group>
+        </el-submenu>
+      </el-menu>
+    </el-aside>
+
+    <el-container>
+      <el-header style="text-align: right; font-size: 12px">
+        <el-dropdown>
+          <i class="el-icon-setting" style="margin-right: 15px"></i>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>查看</el-dropdown-item>
+            <el-dropdown-item>新增</el-dropdown-item>
+            <el-dropdown-item>删除</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+        <span>admin</span>
+      </el-header>
+
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+<script>
+export default {
+  name: "",
+  components: {},
+  data() {
+    return {};
+  },
+  methods: {},
+  computed: {},
+  watch: {},
+};
+</script>
+<style scoped>
+.el-header {
+  background-color: #b3c0d1;
+  color: #333;
+  line-height: 60px;
+}
+
+.el-aside {
+  color: #333;
+}
+</style>
